8ac256cc965faf3a2ee0d7541343e2c276c5de6c
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2023-06-02 12:50:42 -0400

    added standings and live score sections, no API

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -4,28 +4,34 @@ import "../App.css";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-    const [standings, setStandings] = useState([]);
-    const [liveGames, setLiveGames] = useState([]);
+  const [standings, setStandings] = useState([]);
+  const [liveGames, setLiveGames] = useState([]);
 
-}
+  var config = {
+    method: "get",
+    url: "https://api-nba-v1.p.rapidapi.com/games",
+    headers: {
+      "x-rapidapi-key": "bdd2c9af47abe6ae0fd1f59672fcc1a7",
+      "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+    },
+  };
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
-var config = {
-  method: 'get',
-  url: 'https://api-nba-v1.p.rapidapi.com/games',
-  headers: {
-    'x-rapidapi-key': 'bdd2c9af47abe6ae0fd1f59672fcc1a7',
-    'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com'
-  }
+  return (
+    <div className="container">
+      <div className="standings">
+        <h2>Standings...</h2>
+      </div>
+      <div className="live">
+        <h2>Live games...</h2>
+      </div>
+    </div>
+  );
 };
-
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
-
-
-
 export default Home;
