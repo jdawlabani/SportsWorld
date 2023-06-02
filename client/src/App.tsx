8fcafa99b0b04b9ed58a5841bae709bd7d497339
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2023-06-02 12:11:50 -0400

    start work on home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <h1 className="Title">SportsWorld</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/Components/Home.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "../App.css";
+import { Link } from "react-router-dom";
+
+const home = () => {
+    
+}
