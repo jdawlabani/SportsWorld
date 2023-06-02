e4c7190ae3bffd628efcba63dfa11a49e69a774a
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2023-06-02 12:38:02 -0400

    save

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Components/Home'
 
 function App() {
   return (
--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -6,5 +6,26 @@ import { Link } from "react-router-dom";
 const Home = () => {
     const [standings, setStandings] = useState([]);
     const [liveGames, setLiveGames] = useState([]);
-    
+
 }
+
+var config = {
+  method: 'get',
+  url: 'https://api-nba-v1.p.rapidapi.com/games',
+  headers: {
+    'x-rapidapi-key': 'bdd2c9af47abe6ae0fd1f59672fcc1a7',
+    'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com'
+  }
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+
+
+export default Home;
