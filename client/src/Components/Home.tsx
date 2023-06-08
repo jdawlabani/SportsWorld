2f3fe47d065ba691c07591f84d8bc02c3428273c
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2023-06-08 11:28:40 -0400

    continued API work

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -5,13 +5,12 @@ import "../App.css";
 
 const Home = () => {
 // WILL BE USED ONCE API IS CONNECTED
-  const [standings, setStandings] = useState("");
+  const [standings, setStandings] = useState([]);
 //   const [liveGames, setLiveGames] = useState([]);
 
   var config = {
     method: "get",
-    url: "https://v2.nba.api-sports.io/seasons",
-    parameters: "2019",
+    url: "https://v2.nba.api-sports.io/standings?league=standard&season=2022",
     headers: {
       "x-rapidapi-key": "bdd2c9af47abe6ae0fd1f59672fcc1a7",
       "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
@@ -27,20 +26,20 @@ useEffect(()=> {
     axios(config)
     .then((response) => {
       console.log(response.data);
-      setStandings(JSON.stringify(response.data));
+      setStandings(response.data);
 
     })
     .catch((error) => {
       console.log(error);  
     });
-});
+}, []);
 
 
   return (
     <div className="container">
       <div className="standings">
         <h2>Standings...</h2>
-        <h2>{standings}</h2>
+        
       </div>
       <div className="live">
         <h2>Live games...</h2>
