cb7641c5d022e6855778d1dc7ce5a2c54e18ea51
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2023-06-29 15:59:09 -0400

    save

--- client/src/Components/MLB.tsx
+++ client/src/Components/MLB.tsx
@@ -28,32 +28,33 @@ const MLB = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  console.log(apidata)
+  console.log(apidata);
   return (
-    <div className="container">
+    <div className="block">
       <Link className="link" to={"/"}>
         Home
       </Link>
-
-      <h1>This is the MLB page. We are in season and will be updating soon!</h1>
-      <div className="standings">
-        <h2>Standings...</h2>
-        <table className="border">
-          <thead>
-            <tr>
-              <th>Team</th>
-              <th>Wins</th>
-              <th>Losses</th>
-              <th>Win %</th>
-            </tr>
-          </thead>
-          <tbody></tbody>
-        </table>
+        <h1>
+          This is the MLB page. We are in season and will be updating soon!
+        </h1>
+        {/* <div className="standings">
+          <h2>Standings...</h2>
+          <table className="border">
+            <thead>
+              <tr>
+                <th>Team</th>
+                <th>Wins</th>
+                <th>Losses</th>
+                <th>Win %</th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
+        </div>
+        <div className="live">
+          <h2>Live games...</h2>
+        </div> */}
       </div>
-      <div className="live">
-        <h2>Live games...</h2>
-      </div>
-    </div>
   );
 };
 export default MLB;
