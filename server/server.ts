f7b407ed75fbdfb81016a35d3c46eb90383d9152
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2023-07-12 12:12:57 -0400

    save

--- server/server.ts
+++ server/server.ts
@@ -5,8 +5,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());                           
 app.use(express.urlencoded({ extended: true }));
+//configuration file
 require('./config/mongoose.config');
-require('./routes/person.routes')(app);
+//import routes
+require('./routes/team.routes')(app);
 app.listen(8000, () => {
     console.log("Listening at Port 8000")
 })
